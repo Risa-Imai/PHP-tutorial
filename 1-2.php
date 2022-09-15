f3affdf50199288448eed9987a802624201fa364
Risa-Imai <101160553+Risa-Imai@users.noreply.github.com>
2022-09-15 19:17:29 +0900

    add for 1-2.php

---
+++ 1-2.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>PHP-tutorial</title>
+</head>
+<body>
+  <?php
+    $dynamite = "爆豪勝己";
+    $deku = "緑谷出久";
+    echo $dynamite;
+    echo "<br/>";
+    echo $deku;
+  ?>
+</body>
+</html>
